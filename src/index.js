fedd0a9213faf4e9814bd9eabb691ea51f6fdc1a
JohnZBurnett <tuninator@gmail.com>
2018-07-05 09:52:41 -0400

    implement pic display & like count update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  const imageId = 1 //Enter your assigned imageId here
+  const imageId = 8 //Enter your assigned imageId here
+
+  const imgTag = document.getElementById("image");
+  const likesTag = document.getElementById("likes");
+  const commentsList = document.getElementById("comments");
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
@@ -8,7 +12,16 @@ document.addEventListener('DOMContentLoaded', function() {
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
+  const testFetch = fetch(imageURL).then( resp => resp.json() ).then( imgJson => loadAndDisplayData(imgJson));
 
+  function loadAndDisplayData(imgObj) {
+    imgTag.src = `${imgObj.url}`;
+    likesTag.innerText = imgObj.like_count
+    displayComments(imgObj);
+  }
 
+  function displayComments(imgObj) {
+
+  }
+  debugger;
 })
-
