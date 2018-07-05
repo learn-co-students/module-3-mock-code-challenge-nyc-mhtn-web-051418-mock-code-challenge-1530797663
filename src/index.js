ed77d0c2ca6551cff4b6d2c145e90ed4b185d1a2
Alberto Carreras <a.carreras.c@gmail.com>
2018-07-05 13:27:50 -0400

    Add remove

--- src/index.js
+++ src/index.js
@@ -22,13 +22,29 @@ document.addEventListener('DOMContentLoaded', function() {
   const btnIcon = `<img src="http://icons.iconarchive.com/icons/icons8/windows-8/256/Programming-Delete-Sign-icon.pngv" alt="delete button" style="width:5px;height:5px;">`
 
 //static event listeners
-likeBtn.addEventListener("click", function(e) {
+likeBtn.addEventListener("click", function() {
   likesCounter = ++likesCounter;
   likeDisplay.innerHTML = likesCounter;
   saveCounter(likesCounter);
 })
 
+comments.addEventListener("click", function(event) {
+  if (event.target.dataset.action === "deleteComment") {
+    console.log(event.target.id)
+    deleteComment(event.target.id);
+  }
+})
+
+
 //functionality
+  //image functionality
+  function displayImage(r) {
+    const rURL = r.url
+    const rName = r.name
+    imageContainer.innerHTML = `<img src="${rURL}" alt="rName"><h2>"${rName}"</h2>`
+    displayLikes(r.like_count);
+    displayComments(r.comments);
+  }
   //like functionality
   function saveCounter(likesCounter){
     const likesURL = "https://randopic.herokuapp.com/likes";
@@ -45,41 +61,55 @@ likeBtn.addEventListener("click", function(e) {
   }
 
   //comment functionality
+
+  function deleteComment(eventTargetId){
+    const deleteCommentURL = `https://randopic.herokuapp.com/comments/${eventTargetId}`;
+    console.log(deleteCommentURL)
+    fetch(deleteCommentURL, {method: "DELETE"}).then(r=>r.json()).then(init);
+  };
+
+  function buildComment(commentContent, commentId) {
+    return `<div class="commentLine"><li>${commentContent}</li><img class="trashIcon" data-action="deleteComment" id=${commentId} src="https://image.flaticon.com/icons/svg/60/60761.svg" alt="delete button" style="width:10px;height:10px;"></div><br>`;
+  }
+
   function buildComments(respComments){
     const array = respComments.map(function(comment){
-      return `<div class="commentLine"><li>${comment.content}</li><img class="trashIcon" id=${comment.id} src="https://image.flaticon.com/icons/svg/60/60761.svg" alt="delete button" style="width:10px;height:10px;"></div><br>`;
+      return buildComment(comment.content, comment.id);
     })
     return array;
   }
 
   function displayComments(respComments){
-    const builtComment = buildComments(respComments);
-    commentContainer.innerHTML = builtComment.join("");
+    const builtCommentSection = buildComments(respComments);
+    commentContainer.innerHTML = builtCommentSection.join("");
   }
 
-  //image functionality
-  function displayImage(r) {
-    const rURL = r.url
-    const rName = r.name
-    imageContainer.innerHTML = `<img src="${rURL}" alt="rName"><h2>"${rName}"</h2>`
-    displayLikes(r.like_count);
-    displayComments(r.comments);
+  function displayNewComments(commentSubmitted) {
+    const returnedBuiltComment = buildComment(commentSubmitted, "newID")
+    commentContainer.innerHTML += returnedBuiltComment;
   }
 
-  //comment functionality
+  function replaceNewId(r){
+    const newCommentFound = document.getElementById("newID");
+    console.log(newCommentFound);
+    console.log(r.id);
+    newCommentFound.id = r.id;
+  }
+
   commentBtn.addEventListener("click", function(event) {
     event.preventDefault();
     const commentSubmitted = commentInput.value;
-    commentContainer.innerHTML += `<li id="new1">${commentSubmitted}</li>`;
-
+    displayNewComments(commentSubmitted)
     const commentsURL = "https://randopic.herokuapp.com/comments"
     const data = {image_id: 1, content: commentSubmitted}
     const configObj = {method: "POST", headers: {'Accept': 'application/json',
   'Content-Type': 'application/json'}, body: JSON.stringify(data)}
-    fetch(commentsURL, configObj).then(r => r.json()).then(console.log)
+    fetch(commentsURL, configObj).then(r => r.json()).then(r => replaceNewId(r));
   })
 
 //fetch & init
+function init() {
   fetch(imageURL).then(r => r.json()).then( r => displayImage(r) );
-
+}
+init();
 })
